9a4b273eca643a864527305e30e1d4aeb13d6566
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-02 12:41:00 -0300

    new

--- src/Commands/asignar.js
+++ src/Commands/asignar.js
@@ -20,7 +20,7 @@ RONIN_PROVIDER = "https://api.roninchain.com/rpc"
 
 
 module.exports = new Command({
-	name: "retiros",
+	name: "asignar",
 	description: "Shows the price of the slp!",
 	async run(message, args, client) {
         try{
@@ -28,11 +28,10 @@ module.exports = new Command({
                 const web3 = await new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER_FREE));
 
                 //IDs
-                let from_acc=utils.getWalletById(args[1])
                 let to_acc=utils.getWalletById(args[2])
                 //Data
-                if(!utils.isSafe(from_acc) || !utils.isSafe(to_acc))return message.reply(`Una de las wallets esta mal!`);
-                from_acc=from_acc.replace('ronin:','0x')
+                if(!utils.isSafe(to_acc))return message.reply(`Una de las wallets esta mal!`);
+                let from_acc='0x858984a23b440e765f35ff06e896794dc3261c62'
                 to_acc=to_acc.replace('ronin:','0x')
 
                 
@@ -44,13 +43,13 @@ module.exports = new Command({
                 let axie_contract = new web3.eth.Contract(axie_abi,web3.utils.toChecksumAddress(AXIE_CONTRACT))
             
                 //build
-                let axies=await utils.getAxiesIds(from_acc)
-                utils.log(axies.axies.length)
-                for(let i in axies.axies){
-                    let axie_id=axies.axies[i].id
+                let axies_ids=args[1].split(",");
+                console.log(axies_ids)
+                for(let i in axies_ids){
+                    let axie_id=axies_ids[i]
                     console.log('Transfer:'+axie_id)
 
-                    message.reply("Listo para transferir el Axie: "+axie_id+" \n Aguarde un momento...");
+                    message.reply("Listo para transferir el Axie: "+axie_id+" \nAguarde un momento...");
                     let nonce = await web3.eth.getTransactionCount(from_acc, function(error, txCount) { return txCount}); 
                     let myData=axie_contract.methods.safeTransferFrom(
                     (web3.utils.toChecksumAddress(from_acc)),
@@ -68,13 +67,18 @@ module.exports = new Command({
                             data:myData
                     }
 
+                    try{
+                        let signed  = await web3.eth.accounts.signTransaction(trans, from_private)
+                        let tr_raw=await web3.eth.sendSignedTransaction(signed.rawTransaction)
+                        utils.log(tr_raw.status)
                         
-                    let signed  = await web3.eth.accounts.signTransaction(trans, from_private)
-                    let tr_raw=await web3.eth.sendSignedTransaction(signed.rawTransaction)
-                    utils.log(tr_raw.status)
-                    
-                    if(tr_raw.status)utils.log(`Exito!\nhttps://explorer.roninchain.com/tx/${tr_raw.transactionHash}`,message);
-                    else utils.log("ERROR Status False");
+                        if(tr_raw.status)utils.log(`Exito!\nhttps://explorer.roninchain.com/tx/${tr_raw.transactionHash}`,message);
+                        else utils.log("ERROR Status False");
+
+                    }catch(e){
+                        utils.log("Fallo la transfer");
+                        
+                    }
                 }
                 utils.log("Listo!",message);
             }else{
