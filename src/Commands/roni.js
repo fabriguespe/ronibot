58fad54db2e2559a3590622ee2097b86846641a2
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-08 21:34:07 -0300

    new

--- src/Commands/roni.js
+++ src/Commands/roni.js
@@ -43,13 +43,13 @@ module.exports = new Command({
             components: [row]
         })
 		let lascomnd=''
-		const mcollector = thread.createMessageCollector({filter:(m) => m.author.id === message.author.id,max:1,time:20000})
+		const mcollector = thread.createMessageCollector({filter:(m) => m.author.id === message.author.id,max:1,time:30000})
 		mcollector.on('collect', async message => {
 			if(lascomnd=='desasociar')return utils.desasociar(message)
 			else if(lascomnd=='asociar')return utils.asociar(message)
 		});
 
-		const collector = thread.createMessageComponentCollector({ componentType: 'BUTTON', time: 20000 });
+		const collector = thread.createMessageComponentCollector({ componentType: 'BUTTON', time: 30000 });
 		collector.on('collect',  async interaction => {
 			await interaction.deferUpdate();
 			let customId=interaction.customId
