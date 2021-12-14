01f9d846e8f9facf7329210f6d3e595792716b95
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-14 14:20:00 -0300

    new

--- src/Commands/ranking.js
+++ src/Commands/ranking.js
@@ -3,21 +3,63 @@ const path = require('path');
 
 const Command = require("../Structures/Command.js");
 const fetch = require( "node-fetch")
-var DbConnection = require(path.resolve(__dirname, "../Data/db.js"));
+const { MessageEmbed} = require('discord.js');
+const QuickChart = require('quickchart-js');
+const { stat } = require('fs');
 var utils = require(path.resolve(__dirname, "../utils.js"));
+var DbConnection = require(path.resolve(__dirname, "../Data/db.js"));
 
 module.exports = new Command({
-	name: "cron",
+	name: "ranking",
 	description: "Shows the price of the slp!",
 	async run(message, args, client) {
-		if(!utils.esFabri(message))return message.reply('No tienes permisos para correr este comando')
-		
-		utils.log('Timezone!')
-		let db = await DbConnection.Get();
-		let new_data=[]
-		let users=await db.collection('users').find({}).toArray()
-		
-		utils.log('Proceso corrido a las :' +new Date(Date.now()).toISOString()+' con una cantidad de registros: '+users.length,message);
-	
+		if(!utils.esManager(message))return message.reply('No tienes permisos para correr este comando')
+		try{
+			let db = await DbConnection.Get();
+			let users = await db.collection('users').find().toArray()
+			let data_users=[]
+			let count_users=0
+			for(let ii in users){
+				let eluser=users[ii]
+				let stats = await db.collection('stats').find({accountAddress:eluser.accountAddress},  { sort: { cache_last_updated: -1 } }).toArray();
+				users[ii]['mmr_sum']=0
+				users[ii]['slp_sum']=0
+				users[ii]['slp_prom']=0
+				users[ii]['mmr_prom']=0
+				users[ii]['stat_count']=0
+				stats=stats.sort(function(a, b) {return a.cache_last_updated - b.cache_last_updated});
+				for(let i in stats){
+					let stat=stats[i]
+					let anteultimo=stats[i-1]
+					if(stat && anteultimo && anteultimo.in_game_slp!=undefined && stat.in_game_slp!=undefined){
+						if(stat.in_game_slp<anteultimo.in_game_slp)users[ii]['slp_sum']+=stat.in_game_slp
+						else users[ii]['slp_sum']+=stat.in_game_slp-anteultimo.in_game_slp
+						users[ii]['mmr_sum']+=stat['mmr']
+						users[ii]['stat_count']+=1
+					}
+					
+					if(users[ii]['stat_count']>=7)break
+				}
+				
+				users[ii]['slp_prom']=Math.round(users[ii]['slp_sum']/users[ii]['stat_count'])
+				users[ii]['mmr_prom']=Math.round(users[ii]['mmr_sum']/users[ii]['stat_count'])
+				
+
+			}
+			users=users.sort(function(a, b) {return b.slp_prom - a.slp_prom}).slice(0, 10);;
+			console.log(users)
+			let help=""
+			for(let ii in users){
+				let user=users[ii]
+				help+='#'+user.num+" "+user.name+' SLP:'+user.slp_prom+' COPAS:'+user.mmr_prom+'\n'
+			}	
+			
+			let embed = new MessageEmbed().setTitle('Ranking').setDescription(help).setColor('GREEN').setTimestamp()
+			return message.reply({content: ` `,embeds: [embed]})
+
+		}catch(e){
+			utils.log(e.message,message)
+		}
+
 	}
 });
