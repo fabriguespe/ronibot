7a05a24edc03883281defdf51f254424c6299a31
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-14 13:51:04 -0300

    new

--- src/Commands/general.js
+++ src/Commands/general.js
@@ -66,9 +66,6 @@ module.exports = new Command({
 				chart_data.prom_slp.push(Object.values(data_por_dia)[i].slp/count_users)
 				chart_data.prom_mmr.push(Object.values(data_por_dia)[i].mmr/count_users)
 			}
-			utils.log(JSON.stringify(chart_data.prom_mmr),message)
-			utils.log(JSON.stringify(chart_data.prom_slp),message)
-			utils.log(JSON.stringify(chart_data.slp),message)
 
 			let exampleEmbed = new MessageEmbed().setColor('#0099ff')
 			exampleEmbed = exampleEmbed.addFields(
