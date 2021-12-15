adb520a105ac2060e40663b9e8375791c702dafb
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-15 11:16:42 -0300

    new

--- src/Commands/cron.js
+++ src/Commands/cron.js
@@ -30,6 +30,7 @@ module.exports = new Command({
 				data.timestamp.setDate(data.timestamp.getDate() - 1)
 				data.date=data.timestamp.getDate()+'/'+(data.timestamp.getMonth()+1)+'/'+data.timestamp.getFullYear(); 
 				new_data.push(data)
+				
 				await db.collection('stats').insertOne(data)
 			}
 		}catch (e) {
