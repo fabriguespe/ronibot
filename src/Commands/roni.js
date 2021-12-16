7be7184fd19c931f9b3f121daf49a0a9afef03a7
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-16 18:37:14 -0300

    new

--- src/Commands/roni.js
+++ src/Commands/roni.js
@@ -18,7 +18,8 @@ module.exports = new Command({
 
 		let row=new MessageActionRow()
 		if(esPagos){
-			row.addComponents(new MessageButton().setCustomId('cobros').setLabel('🤑 Cobrar').setStyle('SUCCESS'));
+			row.addComponents(new MessageButton().setCustomId('cerrar_ticket').setLabel('🗑️ Cerrar Ticket').setStyle('DANGER'));
+			row.addComponents(new MessageButton().setCustomId('cobros').setLabel('🤑 Pagar}').setStyle('SUCCESS'));
 		}else if(utils.esJugador(message)){
 			row.addComponents(new MessageButton().setCustomId('cerrar_ticket').setLabel('🗑️ Cerrar Ticket').setStyle('DANGER'));
 			row.addComponents(new MessageButton().setCustomId('ticket_soporte').setLabel('👩🏻‍🚒 Hablar con Soporte').setStyle('PRIMARY'));
