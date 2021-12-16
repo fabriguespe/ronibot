e46449f89b486749500ba77f2980a2c3f9539754
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-16 16:01:13 -0300

    new

--- src/utils.js
+++ src/utils.js
@@ -221,7 +221,7 @@ module.exports = {
         return 10
     },
     esJeissonPagos:function(message){
-        return message.author.id==877625345996632095 && message.channel.name.includes('comandos') 
+        return message.author.id==877625345996632095 && message.channel.name.includes('comandos-admin') 
     },
     esFabri:function(message){
         return message.author.id==533994454391062529 && message.channel.id==917380557099380816
