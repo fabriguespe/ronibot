e0654f1457becf955e26e0d9b8c87a70e97048fc
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-22 21:37:28 -0300

    new

--- src/utils.js
+++ src/utils.js
@@ -241,7 +241,7 @@ module.exports = {
         return message.author.id==877625345996632095 && message.channel.name.includes('comandos') 
     },
     esIngresos:function(message){
-        return message.channel.id==917380557099380816
+        return message.channel.id==909165024642203658//canal ingreso
     },
     esFabri:function(message){
         return message.author.id==533994454391062529 && message.channel.id==917380557099380816
