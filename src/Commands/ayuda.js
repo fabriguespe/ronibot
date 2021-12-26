d91138bc1cbe3acef3b3da71fcc1245ab4bfdc4a
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-12-26 18:09:41 -0300

    new

--- src/Commands/ayuda.js
+++ src/Commands/ayuda.js
@@ -14,15 +14,16 @@ module.exports = new Command({
 	description: "Shows the price of the slp!",
 	async run(message, args, client) {
 		let help=""
-		help+="!roni para poder validarte y realizar cobros\n\n"
 		if(utils.esManager(message))help+="!reporte XX para ver info del jugador\n\n"
-		if(utils.esManager(message))help+="!reporte XX slp/copas para ver graficos\n\n"
 		if(utils.esManager(message))help+="!cambio AXIE_ID DESDE_XX HASTA_XX para transferir axies\n\n"
 		if(utils.esFabri(message))help+="!retiro DESDE_XX HASTA_XX para transferir todos los axies\n\n"
 		if(utils.esFabri(message))help+="!aprobar XX NAME para aprobar una entrevista\n\n"
 		if(utils.esFabri(message))help+="!ranking para traer los 10 mejores\n\n"
 		if(utils.esFabri(message))help+="!recupero para traer los 10 peores\n\n"
+		if(utils.esFabri(message))help+="!general para ver el estado de la academia\n\n"
+		if(utils.esFabri(message))help+="!flush ID,ID para retirar cuentas y su slp\n\n"
 		if(utils.esFabri(message))help+="!update XX FIELD VALUE para actualizar (nota,ScholarPayoutAddress)\n\n"
+		help+="!roni para poder validarte y realizar cobros\n\n"
 		let embed = new MessageEmbed().setTitle('Comandos').setDescription(help).setColor('GREEN').setTimestamp()
 		return message.reply({content: ` `,embeds: [embed]})
 		
