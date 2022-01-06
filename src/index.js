92e694e7f72041018337efb4ecaa40bb33a1204d
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-06 09:06:34 -0300

    new

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ client.on("messageCreate", message => {
 	const args = message.content.substring(config.prefix.length).split(/ +/);
 	const command = client.commands.find(cmd => cmd.name == args[0]);
 	
-	if(!(message.channel.name.includes('comandos') || message.channel.name.includes('ingresos') || message.channel.name.includes('soporte') || message.channel.name.includes('anuncios')))return
+	if(!(message.channel.name.includes('comandos') || message.channel.name.includes('chat') || message.channel.name.includes('ingresos') || message.channel.name.includes('soporte') || message.channel.name.includes('anuncios')))return
 	if (!command) return message.channel.send(`${args[0]} is not a valid command!`);
 	command.run(message, args, client);
 });
