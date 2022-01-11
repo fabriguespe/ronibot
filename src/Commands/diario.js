7c6c62300b1a805cae3fd618143715a13e616999
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-10 22:50:26 -0300

    new

--- src/Commands/diario.js
+++ src/Commands/diario.js
@@ -15,7 +15,7 @@ module.exports = new Command({
 		if(!utils.esManager(message))return message.channel.send('No tienes permisos para correr este comando')
 		try{
 			let db = await DbConnection.Get();
-			let limit_prom=-1
+			let limit_prom=2
 			let users = await db.collection('users').find().toArray()
 			for(let ii in users){
 				let eluser=users[ii]
@@ -25,7 +25,7 @@ module.exports = new Command({
 				users[ii]['mmr_prom']=0
 				users[ii]['stat_count']=0
 
-				let stats = await db.collection('stats').find({accountAddress:eluser.accountAddress},  { sort: { cache_last_updated: -1 } }).toArray();
+				let stats = await db.collection('stats').find({accountAddress:eluser.accountAddress},  { sort: { cache_last_updated: -1 } }).limit(limit_prom).toArray();
 				stats=stats.sort(function(a, b) {return a.cache_last_updated - b.cache_last_updated});
 				for(let i in stats){
 					let stat=stats[i]
@@ -53,6 +53,7 @@ module.exports = new Command({
 			users=users.filter(u => u.slp>0 && (u.nota == null || u.nota == undefined || u.nota == 'aprobada'))
 
 			//Top 10 SLP
+			/*
 			let top=users.sort(function(a, b) {return b.slp - a.slp}).slice(0, 10);
 			let help=''
 			for(let ii in top){
@@ -61,7 +62,7 @@ module.exports = new Command({
 			}	
 			let embed = new MessageEmbed().setTitle("MEJORES 10 SLP").setDescription(help).setColor('#3C5D74').setTimestamp()
 			message.channel.send({content: ` `,embeds: [embed]})
-
+			
 			
 			//Top 10 Copas
 			top=users.sort(function(a, b) {return b.mmr - a.mmr}).slice(0, 10);
@@ -83,7 +84,17 @@ module.exports = new Command({
 			}	
 			embed = new MessageEmbed().setTitle("ULTIMOS 10 SLP").setDescription(help).setColor('#574760').setTimestamp()
 			message.channel.send({content: ` `,embeds: [embed]})
+			*/
 			
+			//Top 10 Copas
+			top=users.sort(function(a, b) {return b.mmr - a.mmr})
+			help=''
+			for(let ii in top){
+				let user=top[ii]
+				help+='#'+user.num+" ***"+user.name+'*** '+user.slp+'('+user.mmr+')\n'
+			}	
+			embed = new MessageEmbed().setTitle("TOP 10 COPAS").setDescription(help).setColor('#3C5D74').setTimestamp()
+			message.channel.send({content: ` `,embeds: [embed]})
 		
 
 		}catch(e){
