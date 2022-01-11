7b3d27a564aeaa318e5a4e128e6be4ac859b5fb4
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-10 22:01:28 -0300

    new

---
+++ src/Commands/retirar.js
@@ -0,0 +1,23 @@
+
+const path = require('path');
+const Command = require("../Structures/Command.js");
+const fetch = require( "node-fetch")
+var DbConnection = require(path.resolve(__dirname, "../Data/db.js"));
+var utils = require(path.resolve(__dirname, "../utils.js"));
+const { MessageEmbed} = require('discord.js');
+
+module.exports = new Command({
+	name: "retirar"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
+	async run(message, args, client) {
+		if(!utils.esFabri(message))return message.channel.send('No tienes permisos para correr este comando')
+		if(args.length==2){	
+            let quien=await utils.getUserByNum(args[1])
+			let db = await DbConnection.Get();
+			await db.collection("users").updateOne({ accountAddress:quien.accountAddress},{ $set: {'nota':'retiro'}} )
+			message.channel.send('El jugador fue retirado con exito')
+			let rCanal = message.guild.channels.cache.find(c => c.id == 909165024642203658);//canal ingresos
+			let embed = new MessageEmbed().setTitle('Nuevo Ingreso!').setDescription("Felicitaciones a "+quien.name+"(#"+quien.num+")\nYa puedes escribir !roni para validarte").setColor('GREEN').setTimestamp()
+			rCanal.send({content: ` `,embeds: [embed]})
+		}
+	}
+});
