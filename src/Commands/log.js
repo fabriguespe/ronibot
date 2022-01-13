a40c9cef644a8fe9cb82547e560378fa96d330bf
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-13 10:36:47 -0300

    new

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -62,39 +62,43 @@ module.exports = new Command({
 			for(let i in stats){
 				let undia=stats[i]
 				let fecha=undia.date
-				if(!data_por_dia[fecha])data_por_dia[fecha]={fecha:fecha,aprobado:0,entrevista:0,retiro:0}
-				if(undia.status=='aprobado')data_por_dia[fecha]={fecha:fecha,aprobado:data_por_dia[fecha].aprobado+=1,entrevista:data_por_dia[fecha].entrevista,retiro:data_por_dia[fecha].retiro}
-				if(undia.status=='retiro')data_por_dia[fecha]={fecha:fecha,retiro:data_por_dia[fecha].retiro+=1,entrevista:data_por_dia[fecha].entrevista,aprobado:data_por_dia[fecha].aprobado}
-				if(undia.status=='entrevista')data_por_dia[fecha]={fecha:fecha,entrevista:data_por_dia[fecha].entrevista+=1,aprobado:data_por_dia[fecha].aprobado,retiro:data_por_dia[fecha].retiro}
+				if(!data_por_dia[fecha])data_por_dia[fecha]={fecha:fecha,aprobado:0,aspirante:0,entrevista:0,retiro:0}
+				if(undia.status=='aprobado')data_por_dia[fecha]={fecha:fecha,aprobado:data_por_dia[fecha].aprobado+=1,entrevista:data_por_dia[fecha].entrevista,retiro:data_por_dia[fecha].retiro,aspirante:data_por_dia[fecha].aspirante,}
+				if(undia.status=='retiro')data_por_dia[fecha]={fecha:fecha,retiro:data_por_dia[fecha].retiro+=1,entrevista:data_por_dia[fecha].entrevista,aprobado:data_por_dia[fecha].aprobado,aspirante:data_por_dia[fecha].aspirante,}
+				if(undia.status=='entrevista')data_por_dia[fecha]={fecha:fecha,entrevista:data_por_dia[fecha].entrevista+=1,aprobado:data_por_dia[fecha].aprobado,retiro:data_por_dia[fecha].retiro,aspirante:data_por_dia[fecha].aspirante,}
+				if(undia.status=='aspirante')data_por_dia[fecha]={fecha:fecha,aspirante:data_por_dia[fecha].aspirante+=1,aprobado:data_por_dia[fecha].aprobado,retiro:data_por_dia[fecha].retiro,entrevista:data_por_dia[fecha].entrevista,}
 			}
 			
-			let chart_data={days:[],aprobado:[],entrevista:[],retiro:[]}
+			let chart_data={days:[],aprobado:[],entrevista:[],retiro:[],aspirante:[]}
 			for(let i in data_por_dia){
 				let data=data_por_dia[i]
 
 				chart_data.days.push(data.fecha)
 				chart_data.aprobado.push(data.aprobado)
 				chart_data.entrevista.push(data.entrevista)
+				chart_data.aspirante.push(data.aspirante)
 				chart_data.retiro.push(data.retiro)
 			}
 
-			//console.log(chart_data)
+			console.log(chart_data)
 			let chart = new QuickChart().setConfig({
 				type: 'bar',
 				data: { 
 					labels: chart_data.days,
 					datasets:[
-						{label: 'Aprobado', data: chart_data.aprobado},
-						{label: 'Retiro', data: chart_data.retiro},
+						{label: 'Aspirante', data: chart_data.aspirante},
 						{label: 'Entrevista', data: chart_data.entrevista},
+						{label: 'Aprobado', data: chart_data.aprobado},
+						{label: 'Retiro',color:'black', data: chart_data.retiro},
 					] 
 				},
 			}).setWidth(800).setHeight(400);
 			message.channel.send(`Grafico: ${await chart.getShortUrl()}`);
 		
-		}else if(args[1]=='!aspirante'){
+		}else if(args[1]=='aspirante'){
 			
 			await utils.cambiarEstado(null,'aspirante',message)
+			utils.log('exito')
 		}
 
 	}
