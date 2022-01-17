1c842d3b5956fc67052d66c03568cd30b2420e61
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-17 18:57:46 -0300

    new

--- src/Commands/roni.js
+++ src/Commands/roni.js
@@ -14,6 +14,7 @@ module.exports = new Command({
 		if(args[1] && !esPagos)return message.channel.send('No tienes permiso s para correr este comando')
 		let currentUsers=args[1]?await utils.getUserByNum(args[1]):await utils.getUserByDiscord(message.author.id)
 		if(!Array.isArray(currentUsers))currentUsers=[currentUsers]
+		if(currentUsers.length==0 || !currentUsers)return message.channel.send('No tienes permiso s para correr este comando')
 		for(let i in currentUsers){
 			let currentUser=currentUsers[i]
 			if(!temporal && (!utils.esIngresos(message) && !currentUser))return message.channel.send('Usuario invalido')
