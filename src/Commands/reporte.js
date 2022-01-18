f6a090d3a88d6de310b55416c4ddeef4b5fb1256
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-18 10:33:24 -0300

    new

--- src/Commands/reporte.js
+++ src/Commands/reporte.js
@@ -69,7 +69,7 @@ module.exports = new Command({
 					if(j==0)help=''
 					let log=stats[j]
 					console.log(log)
-					help+='El '+log.date+' se cambio el estado a ***'+log.status+'***'
+					if(log.type=='status_change')help+='El '+log.date+' se cambio el estado a ***'+log.status+'***\n'
 				}
 				exampleEmbed.addFields(
 					{ name: 'Wallet', value: '[Link](https://explorer.roninchain.com/address/'+eluser.accountAddress+")",inline:true},
