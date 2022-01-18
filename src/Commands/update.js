5e5b9bb54de44797b03c0b967f4c2c448ae17ca8
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-18 09:33:34 -0300

    new

--- src/Commands/update.js
+++ src/Commands/update.js
@@ -1,17 +1,15 @@
 
 const path = require('path');
 const Command = require("../Structures/Command.js");
-const fetch = require( "node-fetch")
 var DbConnection = require(path.resolve(__dirname, "../Data/db.js"));
 var utils = require(path.resolve(__dirname, "../utils.js"));
-const { MessageActionRow, MessageButton ,MessageEmbed} = require('discord.js');
 
-const spawn = require('child_process').spawn;
 module.exports = new Command({
 	name: "update"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
 	async run(message, args, client) {
 		if(!(utils.esJeissonPagos(message) || utils.esFabri(message)))return message.channel.send('No tienes permisos para correr este comando')
-		console.log(args)
+	
+		let db = await DbConnection.Get();
 		if(args.length==4){	
             let quien=await utils.getWalletByNum(args[1])
 			let key=args[2]
@@ -21,27 +19,21 @@ module.exports = new Command({
 			let armado={}
 			armado[key]=value
 			let values={ $set: armado }
-			let db = await DbConnection.Get();
 			await db.collection("users").updateOne({ accountAddress:quien},values )
 			message.channel.send('El jugador fue actualizado con exito')
 			//let rCanal = message.guild.channels.cache.find(c => c.id == 917380557099380816);//canal ingresos
 			//if(value=='aprobada')rCanal.send('El jugador fue actualizado con exito')
 		}else if(args[1]=='all'){
 			if(!utils.esFabri(message))return message.channel.send('No tienes permisos para correr este comando')
-			let db = await DbConnection.Get();
 			let users=await db.collection('users-db').find({}).toArray()
 			try{
 				message.channel.send('Se empezara a procesar')
 				for(let i in users){
 					let user=users[i]
 					user.num=user.num.toString()
-					let db = await DbConnection.Get();
 					var myquery = { num:user.num };
 					let find=await db.collection("users").findOne(myquery)
-					if(!find){
-						await db.collection("users").insertOne(user)
-					}
-					console.log(user.num)
+					if(!find)await db.collection("users").insertOne(user)
 				}
 			}catch (e) {
 				utils.log(e)
