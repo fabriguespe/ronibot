f08ee4eda6d857a4144a6a851a9da73c3f82192a
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-02-07 15:48:06 -0300

    new

--- src/Commands/cambio.js
+++ src/Commands/cambio.js
@@ -10,7 +10,7 @@ module.exports = new Command({
 	name: "cambio"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
 	async run(message, args, client) {
 		if(!utils.esManager(message))return message.channel.send('No tienes permisos para correr este comando')
-        if(args.length==4){
+        if(args.length==3){
 
         //IDs
             let user_from=await utils.getUserByNum(args[2])
