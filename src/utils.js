0298c0ede955d3f4a3d4551fb99235927d68db39
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-03-05 09:15:18 -0300

    new

--- src/utils.js
+++ src/utils.js
@@ -482,15 +482,21 @@ module.exports = {
         return message.author.id==533994454391062529 && message.channel.name.includes('comandos-admin')
     },
     async checkAspirante(message){
-        if(!this.isTesting() && this.esManager(message))return
-        if(!this.isTesting() && !message.channel.name.includes('entrevista'))return
-       // console.log(message.author)
-        let db = await DbConnection.Get();
-        let user=await db.collection('users').findOne({discord:message.author.id.toString()})
-        if(user && user.nota=='aspirante'){
-            let num = await db.collection('users').findOne({nota:'libre'})
-            let rCanal = message.guild.channels.cache.find(c => c.id == 903282885971300362);//canal chat managers
-            rCanal.send("!ingreso "+num.num+" "+message.author.usnermae+"#"+message.author.discriminator)
+        try{    
+            console.log('Check aspirante '+message.author.username)
+            if(!this.isTesting() && this.esManager(message))return
+            if(!this.isTesting() && !message.channel.name.includes('entrevista'))return
+            let db = await DbConnection.Get();
+            let user=await db.collection('users').findOne({discord:message.author.id.toString()})
+            if(user && user.nota=='aspirante'){
+                console.log('Entra')
+                let num = await db.collection('users').findOne({nota:'libre'})
+                let rCanal = message.guild.channels.cache.find(c => c.id == 903282885971300362);//canal chat managers
+                
+                rCanal.send("!ingreso "+num.num+" "+message.author.usnermae+"#"+message.author.discriminator)
+            }  
+        }catch(e){
+            this.log(e,message)
         }
     },
     getUserIDByUsername:async function(name,message){
