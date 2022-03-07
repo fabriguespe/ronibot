9b77eb76feca73c5447bb674e1a2e2af7ca7aa8e
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-03-07 09:32:18 -0300

    new

--- src/utils.js
+++ src/utils.js
@@ -322,29 +322,32 @@ module.exports = {
             this.log(e,message)
         }
     },
-    getSLP:async function(from_acc,message,cache=false){
+    getSLP:async function(from_acc,message=null,cache=false){
         try{
-            if(!this.isSafe(from_acc))return message.channel.send(`Una de las wallets esta mal!`);
             from_acc=from_acc.replace('ronin:','0x')  
-   
             if(!cache) {
                 let jdata=await fetch("https://game-api.skymavis.com/game-api/clients/"+from_acc.replace('ronin:','0x')+"/items/1").then(response => response.json()).then(data => { return data});     
                 if(!jdata || !jdata.blockchain_related){
-             
+                    //console.log(jdata)
                     jdata=await fetch("https://game-api.skymavis.com/game-api/clients/"+from_acc.replace('ronin:','0x')+"/items/1").then(response => response.json()).then(data => { return data});  
-                 
                     if(!jdata || !jdata.blockchain_related){   
                         jdata=await fetch("https://game-api.skymavis.com/game-api/clients/"+from_acc.replace('ronin:','0x')+"/items/1").then(response => response.json()).then(data => { return data});  
                         if(!jdata || !jdata.blockchain_related){   
-                            this.log(from_acc)
+                            this.log("error: "+from_acc)
                             return null
                         }
                     }
                 }
-                console.log(jdata.claimable_total)
                 let balance=jdata.blockchain_related.balance
                 let total=jdata.total-jdata.blockchain_related.balance
                 let data= {in_game_slp:total,ronin_slp:balance?balance:0,last_claim:jdata.last_claimed_item_at,has_to_claim:(jdata.claimable_total>0)}
+                
+                try{//MMR
+                    let mmrdata= await fetch("https://game-api.axie.technology/api/v2/"+user.accountAddress.replace('0x','ronin:'), { method: "Get" }).then(res => res.json()).then((json) => { return json})
+                    if(mmrdata.mmr)data.mmr=mmrdata.mmr
+                }catch(e){
+                    utils.log(e,message)
+                }
                 return data
             }else{
                 //url = "https://game-api.axie.technology/api/v1/"+from_acc.replace('0x','ronin:')  ;
@@ -353,7 +356,7 @@ module.exports = {
             return data
 
         }catch(e){
-            this.log(e,message)
+            this.log("ERROR: "+e.message,message)
         }
     },
     claimData:async function(currentUser,message){
