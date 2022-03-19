de697b6d34b00b59b72aaa0beb0930967ba74555
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-03-19 17:52:29 -0300

    new

--- src/Commands/meta.js
+++ src/Commands/meta.js
@@ -97,8 +97,8 @@ module.exports = new Command({
 			msg=''
 
 			
-			for(let i in users)if(users[i].nota=='pro')msg+='#'+users[i].num
-			embed = new MessageEmbed().setTitle('Metas').setDescription(msg).setColor('#0099ff').setTimestamp()
+			for(let i in users)if(users[i].nota=='pro')msg+='#'+users[i].num+' '+users[i].meta+'\n'
+			embed = new MessageEmbed().setTitle('Cuentas Pro').setDescription(msg).setColor('#0099ff').setTimestamp()
 			message.channel.send({content: ` `,embeds: [embed]})
 
 		}catch (e) {
