0aa3aadc482735ac313eb0b271574abb2e64b2fb
fabrizio guespe <fabrizioguespe@fabrizios-MacBook-Air.local>
2022-04-04 15:17:37 +0100

    new

--- src/utils.js
+++ src/utils.js
@@ -158,11 +158,19 @@ module.exports = {
     },
     isFijo(num){
         //Roni experiment
-        return num=='43' || num=='139' || num=='53' || num=='29' || num=='36' || num=='93' || num=='148'
+        //pablo 43,139
+        //jeisson 93
+        //amaloa 148
+        //alejandro 53,29
+        //carlos runner 36
+        //28 rafaerl
+        //82 
+        return num=='43' || num=='139' || num=='53' || num=='29' || num=='36' || num=='93' || num=='148' || num=='28'
     },
     isProFabri(num){
-        return false
-        return num=='43' || num=='186' || num=='187'|| num=='21'  || num=='45'  || num=='94'
+        return false//para recordar mis cuentas
+        //mios son 6 equipos puros y 28 tutis
+        return num=='43' || num=='186' || num=='187'|| num=='45'  || num=='21'  
     },
     cobro:async function(data,message){
 
