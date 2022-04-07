89dc61e852000e5a21605942d9feed9b2d730065
fabrizio guespe <fabrizioguespe@fabrizios-MacBook-Air.local>
2022-04-07 19:59:46 +0100

    new

--- src/Commands/stat.js
+++ src/Commands/stat.js
@@ -14,7 +14,6 @@ module.exports = new Command({
 	name: "stat"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
 	async run(message, args, client) {
 		if(!utils.esManager(message))return message.channel.send('No tienes permisos para correr este comando')
-		if(!utils.esFabri(message) && args[1]=='pro')return message.channel.send('No tienes permisos para correr este comando')
 		
 		try{
 			let db = await DbConnection.Get();
