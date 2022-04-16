99e27279df99ebcf82394612e705069466d611d4
fabrizio guespe <fabrizioguespe@fabrizios-MacBook-Air.local>
2022-04-16 20:31:15 +0100

    new

--- src/Commands/meta.js
+++ src/Commands/meta.js
@@ -118,12 +118,9 @@ module.exports = new Command({
 			
 			msg=''
 			users=users.sort(function(a, b) {return (a.puesto) - (b.puesto)});
-			for(let i in users)if(users[i].nota=='fijo'){
-				
-				let data=await utils.getSLP(users[i].accountAddress,null,false)
-				msg+='#'+users[i].num+' '+users[i].name+' '+users[i].puesto+'\n'
+			for(let i in users)if(users[i].nota=='fijo')msg+='#'+users[i].num+' '+users[i].name+' '+users[i].puesto+'\n'
 
-			}
+			
 			embed = new MessageEmbed().setTitle('Cuentas Fijas').setDescription(msg).setColor('#0099ff').setTimestamp()
 			message.channel.send({content: ` `,embeds: [embed]})
 
