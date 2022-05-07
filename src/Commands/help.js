9ed1ddee31125ae3c7ab300c599793ef6cd6f9c5
fabrizio guespe <fabrizioguespe@fabrizios-MacBook-Air.local>
2022-05-07 12:37:14 +0100

    new

---
+++ src/Commands/help.js
@@ -0,0 +1,34 @@
+/** @format */
+const path = require('path');
+
+const Command = require("../Structures/Command.js");
+const fetch = require( "node-fetch")
+const { MessageEmbed} = require('discord.js');
+const QuickChart = require('quickchart-js');
+const { stat } = require('fs');
+var utils = require(path.resolve(__dirname, "../utils.js"));
+var DbConnection = require(path.resolve(__dirname, "../Data/db.js"));
+
+module.exports = new Command({
+	name: "help"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
+	async run(message, args, client) {
+		let help='!reporte XX para ver info del jugador\n\n
+			!cambio AXIE_ID,AXIE_ID DESDE_XX HASTA_XX para transferir axies\n\n
+			!update XX FIELD VALUE para actualizar (nota,wallet,name)\n\n
+			!ranking para traer los 10 mejores\n\n
+			!lista para traer los 10 mejores\n\n
+			!entrevista para traer los 10 mejores\n\n
+			!general para ver el estado de la academia\n\n
+			!retiro DESDE_XX HASTA_XX para transferir todos los axies\n\n
+			!aprobar XX para aprobar una entrevista\n\n
+			!ingreso  discord_username\n\n
+			!cron flushall \n\n
+			!pagar cant_de_slp/plata_usd DE_ID/BREED HASTA_ID para hacer un pago normal\n\n
+			!pagar todos\n\n
+			!cron totalslp -> para ver el estado de todo'
+
+		let embed = new MessageEmbed().setTitle('Comandos').setDescription(help).setColor('GREEN').setTimestamp()
+		return message.channel.send({content: ` `,embeds: [embed]})
+		
+	}
+});
