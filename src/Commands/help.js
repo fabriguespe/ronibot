08873de8de421a3c1a639183bc4e6d6579bea29d
fabrizio guespe <fabrizioguespe@fabrizios-MacBook-Air.local>
2022-05-07 13:26:36 +0100

    new

--- src/Commands/help.js
+++ src/Commands/help.js
@@ -5,11 +5,8 @@ const { MessageEmbed} = require('discord.js');
 module.exports = new Command({
 	name: "help"+(process.env.LOGNAME=='fabrizioguespe'?'t':''),
 	async run(message, args, client) {
-		let help='!reporte XX para ver info del jugador\n\n'
-		help+='!transfer AXIE_ID,AXIE_ID FROM_ACCOUNT FROM_ACCOUNT\n\n'
-		help+='!roni ACCOUNT\n\n'
-		help+='!cron flushall \n\n"\n\n'
-			
+		let help='You can see comands here\n'
+		help+='https://github.com/fguespe/ronibot#mongodb\n\n'
 
 		let embed = new MessageEmbed().setTitle('Comandos').setDescription(help).setColor('GREEN').setTimestamp()
 		return message.channel.send({content: ` `,embeds: [embed]})
